b62ca8183c180c255575467ff1f224bdbbec1df8
jiho <ghwlchlaks@naver.com>
2018-12-26 21:58:07 +0900

    default boilerplate

---
+++ src/config/mongo.ts
@@ -0,0 +1,1 @@
+export const URL: string = 'mongodb://localhost:27017/test';
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+import * as indexController from './controllers/index';
+
+import * as mongoDBConfig from './config/mongo';
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const mongoUrl = mongoDBConfig.URL;
+mongoose.connect(mongoUrl, {useNewUrlParser: true}, (err: any) => {
+    if (err) {
+        console.log(err.message);
+    } else {
+        console.log('mongodb connected!');
+    }
+});
+
+app.get('/', indexController.index);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
